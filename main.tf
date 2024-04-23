8398b9d4dba503f3f37f841b779ad9836305cd12
anil <anilkumarvallepu1@gmail.com>
2024-04-23 11:39:06 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+variable "cidr_blocks" {
+    description="cidr blocks and names for vpc and subnet"
+    type =list(object({
+      cidr_block=string
+      name= string
+    }))
+}
+
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block =var.cidr_blocks[0].cidr_block
+    tags = {
+      Name=var.cidr_blocks[0].name
+    }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.cidr_blocks[1].cidr_block
+    availability_zone = "ap-south-1a"
+     tags = {
+      Name:var.cidr_blocks[1].name
+    }
+}
+
